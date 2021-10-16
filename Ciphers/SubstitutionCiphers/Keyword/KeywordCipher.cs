6877e0d7a21857377d0650d2bc5d3c69ea4a704f
Michel van Os <michel.van.os@priva.com>
2021-10-16 15:42:47 +0200

    Added implementation of the keyword cipher.

---
+++ Ciphers/SubstitutionCiphers/Keyword/KeywordCipher.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Linq;
+
+namespace Ciphers.SubstitutionCiphers.Keyword
+{
+    /// <summary>
+    /// Implementation of the Keyword Cipher from:
+    /// https://www.braingle.com/brainteasers/codes/keyword.php
+    /// </summary>
+    public class KeywordCipher : IKeywordCipher
+    {
+        private string _keyword;
+        private int maxKeywordCharOffset;
+
+        public KeywordCipher() : this(string.Empty) { }
+
+        public KeywordCipher(string keyword)
+        {
+            _keyword = keyword ?? throw new ArgumentNullException(nameof(keyword));
+        }
+
+        public string Decode(string cipherText)
+        {
+            if (cipherText == null)
+                throw new ArgumentNullException(nameof(cipherText));
+
+            if (_keyword.Length == 0)
+                return cipherText;
+
+            return cipherText.Aggregate("", (current, character) =>
+                current + (!char.IsLetterOrDigit(character)
+                    ? character
+                    : Decode(character)));
+        }
+
+        public string Encode(string plainText)
+        {
+            if (plainText == null)
+                throw new ArgumentNullException(nameof(plainText));
+
+            if (_keyword.Length == 0)
+                return plainText;
+
+            return plainText.Aggregate("", (current, character) =>
+                current + (!char.IsLetterOrDigit(character)
+                    ? character
+                    : Encode(character)));
+        }
+
+        private char Decode(char input)
+        {
+            var startChar = char.IsUpper(input) ? 'A' : 'a';
+            var inputCharOffset = input - startChar;
+            var inputCharLower = (char)(input - startChar + 'a');
+
+            if (_keyword.Contains(inputCharLower))
+            {
+                // Input was replaced by keyword so we assign original character
+                return (char)(_keyword.IndexOf(inputCharLower) + startChar);
+            }
+            else if (inputCharOffset > maxKeywordCharOffset)
+            {
+                // Input was not effected by keyword so we return the input character.
+                return input;
+            }
+            else
+            {
+                // Input was effected by keyword but not part of it.
+                // We assign the original alphabet character
+                return (char)(inputCharOffset + _keyword.Length + startChar);
+            }
+        }
+
+        private char Encode(char input)
+        {
+            var startChar = char.IsUpper(input) ? 'A' : 'a';
+            var inputCharOffset = input - startChar;
+
+            if (inputCharOffset < _keyword.Length)
+            {
+                // Input is part of keyword so we assign keyword character
+                return (char)(_keyword[inputCharOffset] - 'a' + startChar);
+            }
+            else if (inputCharOffset > maxKeywordCharOffset)
+            {
+                // Input is not effected by keyword so we return the input character.
+                return input;
+            }
+            else
+            {
+                // Input is effected by keyword but not part of it.
+                // We assign a replaced alphabet character
+                return (char)(inputCharOffset - _keyword.Length + startChar);
+            }
+        }
+
+        public string GetKeyword() => _keyword;
+
+        public void SetKeyword(string keyword)
+        {
+            if (keyword == null)
+                throw new ArgumentNullException(nameof(keyword));
+
+            _keyword = string.Join(string.Empty, keyword.ToLowerInvariant().Distinct());
+
+            // Precalculations
+            maxKeywordCharOffset = _keyword.Length == 0 ? 0 : _keyword.Max() - 'a';
+        }
+    }
+
+    public interface IKeywordCipher : ICipher
+    {
+        string GetKeyword();
+        void SetKeyword(string keyword);
+    }
+}
---
+++ Ciphers/SubstitutionCiphers/Keyword/KeywordCipherTests.cs
@@ -0,0 +1,106 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace Ciphers.SubstitutionCiphers.Keyword
+{
+    public class KeywordCipherTests
+    {
+        private readonly IKeywordCipher _cipher;
+
+        private const string ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+        private const string ALL_LETTERS = "the quick brown fox jumps over the lazy dog";
+
+        public KeywordCipherTests()
+        {
+            _cipher = new KeywordCipher();
+        }
+
+        /// <summary>
+        /// Verifies that
+        /// - Duplicate letters are removed
+        /// - Order is preserved
+        /// - Keyword is stored in lower case characters
+        /// </summary>
+        [Fact]
+        public void VerifyUniqueKeywordLetters()
+        {
+            _cipher.SetKeyword("babaABAB");
+            _cipher.GetKeyword().ShouldBe("ba");
+        }
+
+        [Fact]
+        public void VerifyWhenKeywordIsEmpty()
+        {
+            // Arrange
+            var message = ALL_LETTERS;
+            _cipher.SetKeyword(string.Empty);
+
+            // Act
+            var encoded = _cipher.Encode(message);
+            var decoded = _cipher.Decode(encoded);
+
+            // Asset
+            encoded.ShouldBe(decoded);
+            decoded.ShouldBe(message);
+        }
+
+        [Fact]
+        public void VerifyWhenKeywordIsAlphabet()
+        {
+            // Arrange
+            var message = ALL_LETTERS;
+            _cipher.SetKeyword(ALPHABET);
+
+            // Act
+            var encoded = _cipher.Encode(message);
+            var decoded = _cipher.Decode(encoded);
+
+            // Asset
+            encoded.ShouldBe(decoded);
+            decoded.ShouldBe(message);
+        }
+
+        /// <re>
+        /// Verifies that
+        /// - Input letters are replaced multiple times by the keyword
+        ///   In this case 'a' is replaced by 'b' multiple times
+        /// - Keyword letters are given a new alphabet letter multiple times
+        ///   In this case 'b' is given the letter 'a'
+        /// - Any other input letter is still functioning
+        ///   In this case 'c' stil has 'c'
+        /// </summary>
+        [Fact]
+        public void VerifyWhenKeywordIsNotEmpty()
+        {
+            // Arrange
+            var message = "abcabc";
+            _cipher.SetKeyword("b");
+
+            // Act
+            var encoded = _cipher.Encode(message);
+            var decoded = _cipher.Decode(encoded);
+
+            // Asset
+            encoded.ShouldNotBe(decoded);
+            encoded.ShouldBe("bacbac");
+            decoded.ShouldBe(message);
+        }
+
+        [Fact]
+        public void VerifyWhenKeywordIsNotEmpty_And_MixedMessageCasing()
+        {
+            // Arrange
+            var message = "abcABC";
+            _cipher.SetKeyword("b");
+
+            // Act
+            var encoded = _cipher.Encode(message);
+            var decoded = _cipher.Decode(encoded);
+
+            // Asset
+            encoded.ShouldNotBe(decoded);
+            encoded.ShouldBe("bacBAC");
+            decoded.ShouldBe(message);
+        }
+    }
+}
