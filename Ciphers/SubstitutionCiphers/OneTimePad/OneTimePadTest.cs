0dca2e748ec554202dd7d7944d3b59e55687fe83
Ilia Opiakin <i.opyakin@outlook.com>
2021-10-17 11:22:27 +0200

    Adding One Time Pad cipher

---
+++ Ciphers/SubstitutionCiphers/OneTimePad/OneTimePadTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Ciphers.SubstitutionCiphers.OneTimePad;
+using Shouldly;
+using Xunit;
+
+namespace Ciphers.SubstitutionCiphers.OneTimePad
+{
+    public class OneTimePadTest
+    {
+        private readonly OneTimePad _oneTimePad;
+        public OneTimePadTest()
+        {
+            _oneTimePad = new OneTimePad();
+        }
+
+        [Fact]
+        public void TestOTP()
+        {
+            var message = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string encodedText = _oneTimePad.Encode(message);
+            _oneTimePad.Decode(encodedText).ShouldBe(message);
+        }
+    }
+
+}
