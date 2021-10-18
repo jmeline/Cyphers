5d2e01b63317ee64e4d79a7c707ccbfd83229c6f
Nemrud Demir <n@nemrud.de>
2021-10-19 01:28:38 +0200

    Added Tap Code

---
+++ Ciphers.Tests/TapCode/TapCodeTests.cs
@@ -0,0 +1,40 @@
+﻿using Shouldly;
+using System.Linq;
+using Xunit;
+
+namespace Ciphers.TapCode
+{
+    public class TapCodeTests
+    {
+        private readonly ICipher _cipher;
+
+        public TapCodeTests()
+        {
+            _cipher = new TapCode();
+        }
+
+        /// <summary>
+        /// Verifies that:
+        /// - Lower and upper case characters are supported
+        /// - Double spaces are added between characters
+        /// - Slashes are added between words
+        /// - Char 'K' gets replaced by 'C'
+        /// </summary>
+        [Fact]
+        public void VerifyMechanics()
+        {
+            // Arrange
+            var inputMessage = "this is a message and K Should Get replaced bY c";
+            var expectedEncoded = ".... ....  .. ...  .. ....  .... ...  /  .. ....  .... ...  /  . .  /  ... ..  . .....  .... ...  .... ...  . .  .. ..  . .....  /  . .  ... ...  . ....  /  . ...  /  .... ...  .. ...  ... ....  .... .....  ... .  . ....  /  .. ..  . .....  .... ....  /  .... ..  . .....  ... .....  ... .  . .  . ...  . .....  . ....  /  . ..  ..... ....  /  . ...";
+            var expectedDecoded = inputMessage.ToUpperInvariant().Select(c => c == 'K' ? 'C' : c);
+
+            // Act
+            var encoded = _cipher.Encode(inputMessage);
+            var decoded = _cipher.Decode(encoded);
+
+            // Asset
+            encoded.ShouldBe(expectedEncoded);
+            decoded.ShouldBe(expectedDecoded);
+        }
+    }
+}
