c7ca319cd297fdcf326f9581636364832de899c2
Federico Tomasi <federico.tomasi@thelevelgroup.com>
2021-10-21 18:35:22 +0200

    Vigenere Cipher and Vigenere Autokey Cipher implementation

---
+++ Ciphers.Tests/SubstitutionCiphers/Vigenere/VigenereAutokeyCipherTests.cs
@@ -0,0 +1,73 @@
+﻿using Ciphers.SubstitutionCiphers.Keyword;
+using Shouldly;
+using Xunit;
+
+namespace Ciphers.SubstitutionCiphers.Vigenere
+{
+    public class VigenereAutokeyCipherTests
+    {
+        private readonly IKeywordCipher _cipher;
+
+        private const string ALL_LETTERS = "the quick brown fox jumps over the lazy dog";
+
+        public VigenereAutokeyCipherTests()
+        {
+            _cipher = new VigenereAutokeyCipher();
+        }
+
+        /// <summary>
+        /// Verifies that
+        /// - Keyword is stored in lower case characters and without special characters (spaces included)
+        /// </summary>
+        [Fact]
+        public void VerifyKeyworkdLowerCaseNoSpecialCharacters()
+        {
+            _cipher.SetKeyword("a B!c,D-e$F");
+            _cipher.GetKeyword().ShouldBe("abcdef");
+        }
+
+        /// <re>
+        /// Verifies that
+        /// - Input letters are replaced multiple times by the keyword
+        ///   In this case 'a' is replaced by 'b' multiple times
+        /// - Keyword letters are given a new alphabet letter multiple times
+        ///   In this case 'b' is given the letter 'a'
+        /// - Any other input letter is still functioning
+        ///   In this case 'c' stil has 'c'
+        /// </summary>
+        [Fact]
+        public void VerifyWhenKeywordIsNotEmpty()
+        {
+            // Arrange
+            var message = ALL_LETTERS;
+            _cipher.SetKeyword("testkeyword");
+
+            // Act
+            var encoded = _cipher.Encode(message);
+            var decoded = _cipher.Decode(encoded);
+
+            // Asset
+            encoded.ShouldNotBe(decoded);
+            // cfr. https://www.boxentriq.com/code-breaking/vigenere-cipher, plain = "the quick brown fox jumps over the lazy dog", key = "testkeyword"
+            encoded.ShouldBe("mlw jemag pirpu jer rwwqj crrw hen fmoq rjk");
+            decoded.ShouldBe(message);
+        }
+
+        [Fact]
+        public void VerifyWhenKeywordIsNotEmpty_And_MixedMessageCasing()
+        {
+            // Arrange
+            var message = "abcABC";
+            _cipher.SetKeyword("b");
+
+            // Act
+            var encoded = _cipher.Encode(message);
+            var decoded = _cipher.Decode(encoded);
+
+            // Asset
+            encoded.ShouldNotBe(decoded);
+            encoded.ShouldBe("bacBAC");
+            decoded.ShouldBe(message);
+        }
+    }
+}
---
+++ Ciphers/SubstitutionCiphers/Vigenere/VigenereCipher.cs
@@ -0,0 +1,22 @@
+﻿namespace Ciphers.SubstitutionCiphers.Vigenere
+{
+    public class VigenereCipher : VigenereAutokeyCipher
+    {
+        private string PadKeyword(int desiredLength)
+        {
+            var kw = _keyword.Length > desiredLength ? _keyword[..desiredLength] : _keyword;
+
+            var idx = 0;
+            while (kw.Length < desiredLength)
+                kw += _keyword[idx++ % _keyword.Length];
+
+            return kw;
+        }
+
+        protected override string GetEncodingKeyword(string plainText) =>
+            PadKeyword(plainText.Length);
+
+        protected override string GetDecodingKeyword(string cipherText) =>
+            PadKeyword(cipherText.Length);
+    }
+}
