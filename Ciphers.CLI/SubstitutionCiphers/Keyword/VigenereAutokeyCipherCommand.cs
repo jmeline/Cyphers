a78120362c8dc46db68dbf9c885dcd3f9f8bf800
Federico Tomasi <federico.tomasi@thelevelgroup.com>
2021-10-22 10:19:25 +0200

    fix namespace command

--- Ciphers.CLI/SubstitutionCiphers/Keyword/VigenereAutokeyCipherCommand.cs
+++ Ciphers.CLI/SubstitutionCiphers/Keyword/VigenereAutokeyCipherCommand.cs
@@ -3,7 +3,7 @@ using System.CommandLine;
 using System.CommandLine.Invocation;
 using Ciphers.SubstitutionCiphers.Vigenere;
 
-namespace Ciphers.CLI.SubstitutionCiphers.Keyword
+namespace Ciphers.CLI.SubstitutionCiphers.Vigenere
 {
     public sealed class VigenereAutokeyCipherCommand : CipherCommand
     {
