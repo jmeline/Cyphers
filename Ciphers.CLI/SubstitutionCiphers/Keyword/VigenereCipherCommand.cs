361f9a1d3d6b71509d026898df3cad47b9c7e155
Federico Tomasi <federico.tomasi@thelevelgroup.com>
2021-10-25 20:32:33 +0200

    fix command

--- Ciphers.CLI/SubstitutionCiphers/Keyword/VigenereCipherCommand.cs
+++ Ciphers.CLI/SubstitutionCiphers/Keyword/VigenereCipherCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CommandLine;
 using System.CommandLine.Invocation;
 using Ciphers.SubstitutionCiphers.Keyword;
 
@@ -6,18 +7,25 @@ namespace Ciphers.CLI.SubstitutionCiphers.Keyword
 {
     public sealed class VigenereCipherCommand : CipherCommand
     {
-        private readonly VigenereAutokeyCipher cipher;
+        private readonly VigenereCipher cipher;
 
         public VigenereCipherCommand()
-            : base("vigenere", "Vigenere cipher.")
+           : base("vigenere", "Vigenere cipher.")
         {
             cipher = new VigenereCipher();
 
-            Handler = CommandHandler.Create<string, string>(HandleCommand);
+            AddOption(new Option<string>(new string[] { "-k", "--keyword" }, "Keyword.")
+            {
+                IsRequired = true
+            });
+
+            Handler = CommandHandler.Create<string, string, string>(HandleCommand);
         }
 
-        private void HandleCommand(string decode, string encode)
+        private void HandleCommand(string decode, string encode, string keyword)
         {
+            cipher.SetKeyword(keyword);
+
             if (!string.IsNullOrEmpty(decode))
                 Console.Write(cipher.Decode(decode));
             else if (!string.IsNullOrEmpty(encode))
